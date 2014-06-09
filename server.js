75538970a8dd1562b273827520e7ad497c4b8013
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-08 22:40:25 -0500

    adding consolidate + swig

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express'),
+  cons = require('consolidate'),
   app = express();
+
+app.engine('html', cons.swig)
 
 app.get('/', function(request, response){
   response.render('index', {'title': "Coming Soon"});
