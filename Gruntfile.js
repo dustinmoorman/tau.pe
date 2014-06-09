641de313c9cbd474baa8ac2f3e3d9acdfa681d6c
dustinmoorman <dustin.moorman@gmail.com>
2014-06-09 11:55:58 -0500

    full uglify removal

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,6 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json')
     });
 
-    // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-
     // Default task(s).
     grunt.registerTask('default');
 
