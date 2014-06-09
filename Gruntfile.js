c0f27923dc3f6c6dee1b8b816c47dd19ef5b09f1
dustinmoorman <dustin.moorman@gmail.com>
2014-06-09 11:55:35 -0500

    full uglify removal

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default');
 
 };
