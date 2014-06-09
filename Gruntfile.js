b76e403a849248dc245c454920b468d5a21701e8
dustinmoorman <dustin.moorman@gmail.com>
2014-06-09 12:48:14 -0500

    wanting to run watch + nodemon concurrently

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,47 @@
 module.exports = function(grunt) {
 
-    // Project configuration.
+    var paths = {
+        js: ['*.js', 'assets/**/*.js', 'assets/js/foundation/*.js','assets/js/foundation/vendor/*.js'],
+        html: ['*.html', 'views/**'],
+        css: ['*.css', 'assets/css/*.css']
+    }
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         nodemon: {
             dev: {
                 script: 'server.js'
             }
+        },
+        watch: {
+            js: {
+                files: paths.js,
+                options: {
+                    livereload: true
+                }
+            },
+            html: {
+                files: paths.html,
+                options: {
+                    livereload: true
+                }
+            },
+            css: {
+                files: paths.css,
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        concurrent: {
+            tasks: ['nodemon', 'watch'],
+            options: {
+                logConcurrentOutput: true
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-nodemon');
-    grunt.registerTask('default', ['nodemon']);
+    grunt.registerTask('default', ['concurrent']);
 
 };
