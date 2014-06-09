473e0d1f240c0980b12ada8877553fee3b2c780a
dustinmoorman <dustin.moorman@gmail.com>
2014-06-09 11:55:07 -0500

    removing uglify for now

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,16 +2,7 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        }
+        pkg: grunt.file.readJSON('package.json')
     });
 
     // Load the plugin that provides the "uglify" task.
