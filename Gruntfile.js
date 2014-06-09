8ad2b1b1027dc3fa9dae86651fd924242e8f5490
dustinmoorman <dustin.moorman@gmail.com>
2014-06-09 11:59:08 -0500

    adding basic nodemon stuffs

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,15 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        nodemon: {
+            dev: {
+                script: 'server.js'
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-nodemon');
 
     // Default task(s).
     grunt.registerTask('default');
