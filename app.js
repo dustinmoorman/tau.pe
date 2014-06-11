e13748f828eb1770e5fa9b0159502f57806b0495
dustinmoorman <dustin.moorman@gmail.com>
2014-06-11 09:35:00 -0500

    clean pt 3

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    cons = require('consolidate'),
+    path = require('path'),
+    app = express();
+
+app.engine('html', cons.swig)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views')
+app.use(express.static(path.join(__dirname, 'assets')))
+
+app.get('/', function(request, response){
+    response.render('index', {'title': "Coming Soon!"});
+});
+
+app.get('*', function(request, response){
+    response.render('404', {});
+});
+
+app.listen(3000);
+console.dir("Started tau.pe");
