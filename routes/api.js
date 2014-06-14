b3c5e009db8fc11ed3cf8998d058d4599c2187b7
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-14 09:12:22 -0500

    api groundwork

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+var MongoClient = require('mongodb').MongoClient;
+
+
+MongoClient.connect('mongodb://localhost:27017/taupe', function(error, db){
+  if(error) throw error;
+
+
+});
+
+
+//@todo GET for retrieving url from mongo
+
+//@todo PUT for creating mongo record for url
+
