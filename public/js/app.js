695319d785eb44bab8f188de57778507db558a8c
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-15 21:12:07 -0500

    moving IndexCtrl to app.js app.controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
 var app = angular.module('taupe', []);
+
+app.controller('IndexCtrl', function ($scope, $http){
+  $scope.long = '';
+  $scope.url  = '';
+  $scope.submit = function(){
+    if($scope.long.length > 0){
+      $http.post('/url',{
+        "url": $scope.long
+      }).success(function(response){
+        $scope.url = 'http://tau.pe/' + response.slug;
+      });
+    }
+  };
+});
+
+
