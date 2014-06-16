5066a3fdd958141341c965d9d20563d9d1635606
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-16 08:33:07 -0500

    removing console logging

--- public/js/app.js
+++ public/js/app.js
@@ -3,17 +3,13 @@ var app = angular.module('taupe', []).config(function($interpolateProvider){
 });
 
 app.controller('IndexCtrl', function ($scope, $http){
-  console.log('controller init');
   $scope.long = '';
   $scope.url  = '';
   $scope.submit = function(){
-    console.log('submit action');
     if($scope.long.length > 0){
-      console.log('http postings');
-	  $http.post('/url',{
+ 	    $http.post('/url',{
         "url": $scope.long
       }).success(function(response){
-        console.log('success: '. response.slug);
         $scope.url = 'http://tau.pe/' + response.slug;
       });
     }
