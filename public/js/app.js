183533637731ae26ad65d8066d7366b8c49306bd
dustinmoorman <dustin.moorman@gmail.com>
2014-06-16 08:21:04 -0500

    this might be a conflict with angular / swig

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,19 @@
-var app = angular.module('taupe', []);
-
-console.log('included');
+var app = angular.module('taupe', []).config(function($interpolateProvider){
+  $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
 
 app.controller('IndexCtrl', function ($scope, $http){
+  console.log('controller init');
   $scope.long = '';
   $scope.url  = '';
   $scope.submit = function(){
+    console.log('submit action');
     if($scope.long.length > 0){
-			$http.post('/url',{
+      console.log('http postings');
+	  $http.post('/url',{
         "url": $scope.long
       }).success(function(response){
+        console.log('success: '. response.slug);
         $scope.url = 'http://tau.pe/' + response.slug;
       });
     }
