975b690ffde6f4c4628e1202f5c435a63da3aff7
dustinmoorman <dustin.moorman@gmail.com>
2014-06-19 14:35:25 -0500

    damn you jessie

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ app.controller('IndexCtrl', function ($scope, $http){
     var urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
     if(urlRegex.test($scope.long)){
       if($scope.long.length > 0){
-        if(/\b(http|https):\/\/tau\.pe\b/.test($scope.long)){
+        if(/\b(http|https):(\/\/|\/\/www.)tau\.pe\b/.test($scope.long)){
           $scope.warn = "hey now..";
         } else {
           $http.post('/url',{
