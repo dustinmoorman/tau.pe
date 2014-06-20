922e36b3c478661c80e641eb17fabfc4c9b9418f
dustinmoorman <dustin.moorman@gmail.com>
2014-06-20 07:24:08 -0500

    unused callback

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ var assert = require("assert");
 var http = require("http");
 
 describe('api', function(){
-  describe('#getUrl()', function(done){
+  describe('#getUrl()', function(){
     it('should return the URL for the supplied slug', function(done){
 
 		  http.get({
