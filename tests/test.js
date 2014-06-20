a0a3d89ca156997930d834f510c1aae135f79578
dustinmoorman <dustin.moorman@gmail.com>
2014-06-20 07:31:44 -0500

    changing description of API GET test

--- tests/test.js
+++ tests/test.js
@@ -2,7 +2,7 @@ var assert = require("assert");
 var http = require("http");
 
 describe('api', function(){
-  describe('#getUrl()', function(){
+  describe('GET /url/:slug', function(){
     it('should return the URL for the supplied slug', function(done){
 		  http.get({
 				host: 'www.tau.pe',
