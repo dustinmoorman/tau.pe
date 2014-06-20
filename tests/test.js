c62e6b53fa041ec3b15c34fdd7791aeb440952b7
dustinmoorman <dustin.moorman@gmail.com>
2014-06-20 07:31:14 -0500

    loosing these spaces

--- tests/test.js
+++ tests/test.js
@@ -4,17 +4,14 @@ var http = require("http");
 describe('api', function(){
   describe('#getUrl()', function(){
     it('should return the URL for the supplied slug', function(done){
-
 		  http.get({
 				host: 'www.tau.pe',
 				path: '/url/GB2SL'
 			},function(response){
 				var data = "";
-
 				response.on("data", function(chunk){
 					if(chunk.length > 0) data += chunk;
 				});
-
 				response.on("end", function(){
 					var url = '';
 					if(data != null){
@@ -26,7 +23,6 @@ describe('api', function(){
 					assert.equal('http://softlayer.com/', url);
 					done();
 				});
-
 			}).end();
 		})
   })
