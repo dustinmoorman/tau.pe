17780ed698eab5753357483f292800d12d1a1338
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-22 18:21:13 -0500

    ghetto tabz

--- tests/test.js
+++ tests/test.js
@@ -6,25 +6,25 @@ describe('api', function(){
     it('should return the URL for the supplied slug', function(done){
       http.get({
         host: 'www.tau.pe',
-	path: '/url/GB2SL'
-	},function(response){
-	  var data = "";
-	  response.on("data", function(chunk){
-	    if(chunk.length > 0) data += chunk;
-	  });
-	  response.on("end", function(){
-	    var url = '';
-	    if(data != null){
-	      var json = JSON.parse(data);
-	      if(json != null){
-		url = json.url;
-	      }
-	    }
-	    assert.equal('http://softlayer.com/', url);
-	    done();
-	  });
-	}).end();
-     })
+	      path: '/url/GB2SL'
+      },function(response){
+        var data = "";
+        response.on("data", function(chunk){
+          if(chunk.length > 0) data += chunk;
+        });
+        response.on("end", function(){
+          var url = '';
+          if(data != null){
+            var json = JSON.parse(data);
+            if(json != null){
+              url = json.url;
+            }
+          }
+          assert.equal('http://softlayer.com/', url);
+          done();
+        });
+      }).end();
+    })
   })
 });
 
