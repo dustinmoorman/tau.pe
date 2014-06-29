cce85c74651733933bbb8cddb429972525242988
Dustin Moorman <dustin.moorman@gmail.com>
2014-06-29 00:46:18 -0500

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -58,7 +58,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-nodemon');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-concurrent');
-    
+
     grunt.registerTask('default', ['concurrent', 'jshint']);
 
 };
